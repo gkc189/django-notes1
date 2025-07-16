470c91ed68e9b697fe0ee928cb9e15cf53f27672
gkohne <gk@bogus.local>
2025-07-15 21:36:54 -0500

    Added ability to make notes public or private, and pertain to the users that wrote them. Also made changes to num-likes and visibility as required POST methods else 404 error.

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Notes(models.Model):
@@ -6,3 +7,7 @@ class Notes(models.Model):
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveIntegerField(default=0)
+    # Cascade means: When this note is delete, if user associated with this note wasç deleted, 
+    # delete all notes corresponding to this user.
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    public = models.BooleanField(default=False)
--- notes/views.py
+++ notes/views.py
@@ -2,17 +2,28 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponseRedirect
 from django.views.generic import CreateView, ListView, UpdateView, DetailView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import NotesForm
 from django.urls import reverse
 
 # Create your views here.
 from .models import Notes
 
+def change_visibility_view(request, pk):
+    if request.method == 'POST':
+        note = get_object_or_404(Notes, pk=pk)
+        note.public = not note.public
+        note.save()
+        return HttpResponseRedirect(reverse("notes.detail",args=(pk,)))
+    raise Http404
+
 def add_like_view(request, pk):
-    note = get_object_or_404(Notes, pk=pk)
-    note.likes += 1
-    note.save()
-    return HttpResponseRedirect(reverse("notes.detail",args=(pk,)))
+    if request.method == 'POST':
+        note = get_object_or_404(Notes, pk=pk)
+        note.likes += 1
+        note.save()
+        return HttpResponseRedirect(reverse("notes.detail",args=(pk,)))
+    raise Http404
 
 class NotesDeleteView(DeleteView):
     model = Notes
@@ -29,9 +40,19 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
